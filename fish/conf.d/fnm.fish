33bd93856efa64f129968e03d3e73a002fa35753
Ari Juanda <juandari@Aris-MacBook-Pro.local>
2026-07-29 18:32:45 +0700

    feat: add herdr and some adjustments

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -5,3 +5,9 @@ if [ -d "$FNM_PATH" ]
   set PATH "$FNM_PATH" $PATH
   fnm env --shell fish | source
 end
+
+# fnm
+set FNM_PATH "/opt/homebrew/opt/fnm/bin"
+if [ -d "$FNM_PATH" ]
+  fnm env --shell fish | source
+end
